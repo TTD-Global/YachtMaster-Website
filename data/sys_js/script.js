3b6c77e0557b9121e7d9e64d7eb9cad6b25d1fe8
Verwihart <rescal@gmx.de>
2024-02-08 18:18:14 +0700

    Fleet and slider Changes

--- data/sys_js/script.js
+++ data/sys_js/script.js
@@ -1,3 +1,16 @@
 $('.navbar-toggler').click(function(){
     $('.navbar').toggleClass("menue-on");
   });
+
+  new Splide( '#image-carousel', {
+    perPage: 3,
+   
+    type     : 'loop',
+    focus    : 0,
+
+		breakpoints: {
+			768: {
+				perPage: 1,
+			},
+		},
+  } ).mount();
